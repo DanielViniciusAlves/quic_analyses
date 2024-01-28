a90acb45a2b37e8ff608b7a15563e677f09ecfd8
DanielViniciusAlves <danielribeiroalves0@gmail.com>
2024-01-28 20:48:23 -0300

    Adicionando conexão do tipo SSL ao cliente - #12

---
+++ apps/client/lib/client/connection/ssl_handler.ex
@@ -0,0 +1,39 @@
+defmodule Client.Connection.SslHandler do
+  alias Client.Error.ErrorHandler, as: Error
+  alias :ssl, as: Ssl
+  require Logger
+
+  @spec connect(state :: map()) :: {:ok, state :: map()} | {:stop, Error.t()}
+  def connect(state) do
+    host = Application.get_env(:server, :host)
+    port = Application.get_env(:server, :port)
+
+    with {:ok, socket} <-
+           Ssl.connect(host, port, [:binary, active: true, verify: :verify_none], 5000) do
+      {:ok, %{state | connection_handler: Client.Connection.SslHandler, socket: socket}}
+    else
+      response ->
+        IO.inspect(response)
+        {:stop, Error.exception(:connection)}
+    end
+  end
+
+  @spec send(state :: map(), payload :: binary()) :: {:ok, state :: map()} | {:error, Error.t()}
+  def send(state, payload) do
+    case Ssl.send(state.socket, payload) do
+      :ok ->
+        {:ok, state}
+
+      {:error, reason} ->
+        Logger.error(reason)
+        {:error, Error.exception(:send)}
+    end
+  end
+
+  @spec handle_connection(message :: any(), state :: map()) ::
+          {:ok, state :: map()} | {:error, Error.t()}
+  def handle_connection(message, state) do
+    IO.inspect(message)
+    {:ok, state}
+  end
+end
